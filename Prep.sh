6d746115a75097628a118c1a78ac000b1801bbfd
sargundy <sgundy.gma@gmail.com>
2024-08-01 16:46:42 -0400

    Update Prep.sh

--- Prep.sh
+++ Prep.sh
@@ -1,2 +1,2 @@
-cat /home/sgundy/projects/rrg-shaferab/sgundy/thesis/deer_modern/mito/SNPs/all-modern-$sp.fasta > all-$sp.fasta &&
-cat /home/sgundy/projects/rrg-shaferab/sgundy/thesis/deer_ancient/mito/SNPs/all-ancient-$sp.fasta >> all-$sp.fasta
+cat /home/sgundy/projects/rrg-shaferab/sgundy/thesis/deer_modern/mito/SNPs/all-modern-$sp.fasta > all-deer-MT.fasta &&
+cat /home/sgundy/projects/rrg-shaferab/sgundy/thesis/deer_ancient/mito/SNPs/all-ancient-$sp.fasta >> all-deer_MT.fasta
