4150b08f1febf5ad34d30d32c92bd7aab8751682
sargundy <sgundy.gma@gmail.com>
2024-08-01 16:19:35 -0400

    Update Alignment.sh

--- Alignment.sh
+++ Alignment.sh
@@ -4,4 +4,4 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=30000M
 #SBATCH --time=3-00:00
-megacc -a /home/sgundy/projects/rrg-shaferab/sgundy/Analyses/clustal_align_nucleotide.mao -d /home/sgundy/projects/rrg-shaferab/sgundy/deer_modern/mito/SNP/all_deer_MT.fasta -o /Users/saragunderson/Desktop/Academics/Thesis/Bioinformatics/Alignments/all_deer_MT_clustal.fasta
+megacc -a /home/sgundy/projects/rrg-shaferab/sgundy/scripts/Analyses/clustal_align_nucleotide.mao -d /home/sgundy/projects/rrg-shaferab/sgundy/thesis/all-$sp.fasta -o /Users/saragunderson/Desktop/Academics/Thesis/Bioinformatics/Alignments/all-$sp-clustalw.fasta
