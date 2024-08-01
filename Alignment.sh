81b78534afeed8958807297a0072e095c709f8b2
sargundy <sgundy.gma@gmail.com>
2024-08-01 16:52:01 -0400

    Update Alignment.sh

--- Alignment.sh
+++ Alignment.sh
@@ -7,4 +7,4 @@
 #SBATCH --job-name=clustalw
 #SBATCH --output=clustalw-deer_MT.out
 
-megacc -a /home/sgundy/projects/rrg-shaferab/sgundy/thesis/alignments/clustal_align_nucleotide.mao -d /home/sgundy/projects/rrg-shaferab/sgundy/thesis/all-deer-MT.fasta -o /Users/saragunderson/Desktop/Academics/Thesis/Bioinformatics/Alignments/all-deer_MT-clustalw.fasta -f Fasta
+/home/sgundy/projects/rrg-shaferab/sgundy/software/MEGA11/megacc -a /home/sgundy/projects/rrg-shaferab/sgundy/thesis/alignments/clustal_align_nucleotide.mao -d /home/sgundy/projects/rrg-shaferab/sgundy/thesis/all-deer-MT.fasta -o /Users/saragunderson/Desktop/Academics/Thesis/Bioinformatics/Alignments/all-deer_MT-clustalw.fasta -f Fasta
