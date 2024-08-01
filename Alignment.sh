17af34631b6fcb5dafe64a3f29b2282c9f4a2dfe
sargundy <sgundy.gma@gmail.com>
2024-08-01 16:41:11 -0400

    Update Alignment.sh

--- Alignment.sh
+++ Alignment.sh
@@ -4,4 +4,4 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=30000M
 #SBATCH --time=3-00:00
-megacc -a /home/sgundy/projects/rrg-shaferab/sgundy/scripts/Analyses/clustal_align_nucleotide.mao -d /home/sgundy/projects/rrg-shaferab/sgundy/thesis/all-$sp.fasta -o /Users/saragunderson/Desktop/Academics/Thesis/Bioinformatics/Alignments/all-$sp-clustalw.fasta
+megacc -a /home/sgundy/projects/rrg-shaferab/sgundy/thesis/alignments/clustal_align_nucleotide.mao -d /home/sgundy/projects/rrg-shaferab/sgundy/thesis/all-OVirginianus_MT.fasta -o /Users/saragunderson/Desktop/Academics/Thesis/Bioinformatics/Alignments/all-OVirginianus_MT-clustalw.fasta
