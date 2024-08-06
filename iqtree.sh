8ab262cb6b9c5d92cd611aac0b90c07f110309aa
sargundy <sgundy.gma@gmail.com>
2024-08-06 11:35:48 -0400

    Update iqtree.sh

--- iqtree.sh
+++ iqtree.sh
@@ -1,1 +1,3 @@
 bin/iqtree2 -s /Users/saragunderson/Desktop/Academics/Thesis/Bioinformatics/Alignments/test-clustal-noAmbigGapSingleMiss.fas
+
+./iqtree2 -s /Users/saragunderson/Desktop/Academics/Thesis/Bioinformatics/Alignments/test-clustal-noAmbigGapSingleMiss3.fas -m TEST -bb 1000 -alrt 1000 -redo -bnni
