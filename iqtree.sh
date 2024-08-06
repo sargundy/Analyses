159ad211847a9f4836277b9122c2c6dbb8110fd7
sargundy <sgundy.gma@gmail.com>
2024-08-06 16:23:33 -0400

    Update iqtree.sh

--- iqtree.sh
+++ iqtree.sh
@@ -1,3 +1,3 @@
 bin/iqtree2 -s /Users/saragunderson/Desktop/Academics/Thesis/Bioinformatics/Alignments/test-clustal-noAmbigGapSingleMiss.fas
 
-./iqtree2 -s /Users/saragunderson/Desktop/Academics/Thesis/Bioinformatics/Alignments/test-clustal-noAmbigGapSingleMiss3.fas -m TEST -bb 1000 -alrt 1000 -redo -bnni
+./iqtree2 -s /Users/saragunderson/Desktop/Academics/Thesis/Bioinformatics/Alignments/mafft_all_deer_mt_w_caribou_conserved_filtered.fas -bb 1000 -alrt 1000 -redo -bnni --prefix /Users/saragunderson/Desktop/Academics/Thesis/Bioinformatics/Phylogenies/mafft_all_deer_mt_w_caribou_conserved_filtered_modelfinder
